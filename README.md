82f7dc85e8f2462fbe4080deb15568e824ffa156
Keziah Biermann <development@kizzycode.de>
2018-02-20 18:07:45 +0100

    Initial commit :D

---
+++ README.md
@@ -0,0 +1,16 @@
+[![License](https://img.shields.io/badge/License-BSD%202--Clause-blue.svg)](https://opensource.org/licenses/BSD-2-Clause)
+
+# About
+This library provides some structs and macros to create stackable/traceable errors for your own custom error-kinds.
+
+We provide some macros to create and "throw" an error with a custom kind `T` and optionally a custom description and/or
+a previous `Error<T>` (even for different types of `T`).
+
+Additionally we provide two macros to `try!` an expression and either __convert__ the resulting error into your
+`Error<T>` or to __include__ the resulting error as sub-error into a new `Error<T>` 
+ 
+# Build Library and Documentation
+To build and open the documentation, go into the projects root-directory and run `cargo doc --open`.
+
+To build the library, go into the projects root-directory and run `cargo build --release`; you can find the build in
+target/release.
---
+++ src/lib.rs
@@ -0,0 +1,143 @@
+#[derive(Debug)]
+/// A generic-error that contains the serialized error-kind, description, the position (file, line)
+/// and an optional sub-error
+pub struct WrappedError {
+	pub kind_repr: String,
+	pub description: String,
+	pub file: &'static str,
+	pub line: u32,
+	pub sub_error: Option<std::rc::Rc<WrappedError>>
+}
+impl<T: std::fmt::Debug> From<Error<T>> for WrappedError {
+	fn from(error: Error<T>) -> Self {
+		WrappedError {
+			kind_repr: format!("{:?}", error.kind), description: error.description,
+			file: error.file, line: error.line,
+			sub_error: error.sub_error
+		}
+	}
+}
+impl ToString for WrappedError {
+	/// Converts the error into a human-readable description ("pretty-print")
+	fn to_string(&self) -> String {
+		// Serialize error and sub-error (if any)
+		let mut string = format!("{} ({}) at {}:{}", self.description, self.kind_repr, self.file, self.line);
+		if let Some(ref sub_error) = self.sub_error { string += "\n    : "; string += &sub_error.to_string(); }
+		string
+	}
+}
+
+
+
+#[derive(Debug)]
+/// A typed-error that contains the error-kind, description, the position (file, line) and an
+/// optional sub-error
+pub struct Error<T: std::fmt::Debug> {
+	pub kind: T,
+	pub description: String,
+	pub file: &'static str,
+	pub line: u32,
+	pub sub_error: Option<std::rc::Rc<WrappedError>>
+}
+impl<T: std::fmt::Debug> Error<T> {
+	/// Creates a new error with an explicit description
+	///
+	/// _Note: This function is not intended for direct use; take a look at the `new_err!()`-macro
+	/// instead_
+	pub fn with_kind_desc<S: ToString>(kind: T, description: S, file: &'static str, line: u32) -> Self {
+		Error{ kind, description: description.to_string(), file, line, sub_error: None }
+	}
+	/// Creates a new error
+	///
+	/// _Note: This function is not intended for direct use; take a look at the `new_err!()`-macro
+	/// instead_
+	pub fn with_kind(kind: T, file: &'static str, line: u32) -> Self {
+		let description = format!("{:?}", kind);
+		Error::with_kind_desc(kind, description, file, line)
+	}
+	
+	/// Creates a new error with an explicit description and a sub-error
+	///
+	/// _Note: This function is not intended for direct use; take a look at the `rethrow_err!()`-
+	/// macro instead_
+	pub fn propagate_with_kind_desc<S: ToString>(kind: T, description: S, sub_error: WrappedError, file: &'static str, line: u32) -> Self {
+		Error{ kind, description: description.to_string(), file, line, sub_error: Some(std::rc::Rc::new(sub_error)) }
+	}
+	/// Creates a new error with a sub-error
+	///
+	/// _Note: This function is not intended for direct use; take a look at the `rethrow_err!()`-
+	/// macro instead_
+	pub fn propagate_with_kind(kind: T, sub_error: WrappedError, file: &'static str, line: u32) -> Self {
+		let description = format!("{:?}", kind);
+		Error::propagate_with_kind_desc(kind, description, sub_error, file, line)
+	}
+}
+impl<T: std::fmt::Debug> ToString for Error<T> {
+	/// Converts the error into a human-readable description ("pretty-print")
+	fn to_string(&self) -> String {
+		// Serialize error and sub-error (if any)
+		let mut string = format!("{} ({:?}) at {}:{}", self.description, self.kind, self.file, self.line);
+		if let Some(ref sub_error) = self.sub_error { string += "\n    : "; string += &sub_error.to_string(); }
+		string
+	}
+}
+
+
+
+#[macro_export]
+/// Creates a new error and returns it (`return Err(created_error)`)
+///
+/// Use `throw_err!(error_kind)` to create an error with an automatically created description or use
+/// `throw_err!(error_kind, description)` to provide an explicit description
+macro_rules! throw_err {
+    ($kind:expr, $description:expr) => (return Err($crate::Error::with_kind_desc($kind, $description, file!(), line!())));
+    ($kind:expr) => (return Err($crate::Error::with_kind($kind, file!(), line!())));
+}
+
+#[macro_export]
+/// Creates a new error with a sub-error and returns it (`return Err(created_error)`)
+///
+/// Use `rethrow_err!(error_kind, error)` to create an error with an automatically created
+/// description or use `rethrow_err!(error_kind, description, error)` to provide an explicit
+/// description
+macro_rules! rethrow_err {
+    ($kind:expr, $description:expr, $suberr:expr) =>
+    	(return Err($crate::Error::propagate_with_kind_desc($kind, $description, $suberr.into(), file!(), line!())));
+    ($kind:expr, $suberr:expr) =>
+    	(return Err($crate::Error::propagate_with_kind($kind, $suberr.into(), file!(), line!())));
+}
+
+#[macro_export]
+/// Runs an expression and returns either the unwrapped result or creates a new error with the
+/// returned error as sub-error and returns the new error (`return Err(Error<T>)`)
+///
+/// Use `try_rethrow_err!(expression, error_kind)` to create an error with an automatically created
+/// description or use `try_rethrow_err!(expression, error_kind, description)` to provide an
+/// explicit description
+macro_rules! try_rethrow_err {
+	($code:expr, $kind:expr, $description:expr) => (match $code {
+		Ok(result) => result,
+		Err(error) => return Err($crate::Error::propagate_with_kind_desc($kind, $description, error.into(), file!(), line!()))
+	});
+	($code:expr, $kind:expr) => (match $code {
+		Ok(result) => result,
+		Err(error) => return Err($crate::Error::propagate_with_kind($kind, error.into(), file!(), line!()))
+	});
+}
+
+#[macro_export]
+/// Runs an expression and returns either the unwrapped result or converts the error __into__ the
+/// matching `Error<T>` (using a `From`-trait) and returns it (`return Err(Error<T>)`)
+///
+/// Use `try_wrap_err!(expression)` to create an error with an automatically created description or
+/// use `try_wrap_err!(expression, description)` to provide an explicit description
+macro_rules! try_convert_err {
+    ($code:expr, $description:expr) => (match $code {
+		Ok(result) => result,
+		Err(error) => throw_err!(error.into(), $description)
+	});
+	($code:expr) => (match $code {
+		Ok(result) => result,
+		Err(error) => throw_err!(error.into())
+	});
+}
